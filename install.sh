be963bb8794d6bb6bdd9075f0d8176ab9b71eef9
Jack Cassidy <j.cassidy45@gmail.com>
2020-10-25 14:27:03 +0000

    fix: Blatantly broken install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-cmd="python /usr/local/bin/report-sshd-attempts.py > /usr/local/log/report-sshd-attempts.log 2>&1"
+sudo ln -sf $(pwd)/report-sshd-attempts.py /usr/local/bin
+cmd="PYTHONPATH=$HOME/.pyenv/versions/3.8.0/lib/python3.8/site-packages python3 /usr/local/bin/report-sshd-attempts.py > /usr/local/log/report-sshd-attempts.log 2>&1"
 cron_cmd="@daily $cmd"
 ( crontab -l 2>/dev/null | grep -v -F "$cmd" ; echo "$cron_cmd" ) | crontab -
 
